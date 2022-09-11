b2e703c7f2e97d8e9527983b8c16637ae65077a8
Petri Hirvonen <petenez@users.noreply.github.com>
2022-09-11 11:42:37 +0300

    Delete demos/quench directory

--- demos/quench/plot.sh
+++
@@ -1,5 +0,0 @@
-for (( i = 0 ; i <= 10000 ; i += 1 )) ; do
-  if [[ -e output-baboon-$i.rgb ]] && [[ ! -e baboon-quenched-$i.png ]] ; then
-  	./pngwriter output-baboon-$i.rgb baboon-quenched-$i.png
-  fi
-done
